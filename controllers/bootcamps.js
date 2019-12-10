fc2c1e35f586d5fba1c3a0b94d8e02fe63537f2f
Cody Choi <cchoi_2294@hotmail.com>
2019-12-09 22:46:19 -0500

    added seeder to import and delete JSON data, and added query filtering for getBootcamps

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,13 +1,22 @@
 const Bootcamp = require('../models/Bootcamp');
 const ErrorResponse = require('../utils/errorResponse');
+const geocoder = require('../utils/geocoder');
 
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
 
 exports.getBootcamps = async (req, res, next) => {
+  let query;
+
+  let queryStr = JSON.stringify(req.query);
+
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+  console.log(queryStr);
+  query = Bootcamp.find(JSON.parse(queryStr));
+
   try {
-    const bootcamps = await Bootcamp.find();
+    const bootcamps = await query;
     res.status(200).json({
       success: true,
       count: bootcamps.length,
@@ -98,6 +107,36 @@ exports.deleteBootcamp = async (req, res, next) => {
     }
 
     res.status(200).json({ success: true, data: {} });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// @desc    Get bootcamps within a radius
+// @route   Get /api/v1/bootcamps/radius/:zipcode/:distance
+// @access  Private
+
+exports.getBootcampsInRadius = async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+  try {
+    const loc = await geocoder.geocode(zipcode);
+    const lat = loc[0].latitude;
+    const lng = loc[0].longitude;
+
+    // Calc radius using radians
+    // Divide distance by radius of Earth
+    // Earth Radius = 3963 mi / 6378 km
+    const radius = distance / 3963;
+
+    const bootcamps = await Bootcamp.find({
+      location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+    });
+
+    res.status(200).json({
+      success: true,
+      count: bootcamps.length,
+      data: bootcamps,
+    });
   } catch (err) {
     next(err);
   }
